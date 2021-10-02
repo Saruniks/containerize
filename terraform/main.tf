15cb09fd7d64364b43f9406b194de30bc0bbc016
sarunas.gincas <sarunikss@gmail.com>
2021-10-02 13:25:22 +0300

    Terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,11 @@ provider "aws" {
     region  = "us-east-2"
 }
 
-resource "aws_instance" "minimal" {
+resource "aws_instance" "minimal_instance" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "Minimal instance"
+    }
 }
